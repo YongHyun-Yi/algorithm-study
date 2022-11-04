e66ce66bc0f107d1ad900097c83b38da2d26870f
YongHyun-Yi <dldydgus1995@naver.com>
2022-11-04 22:36:17 +0900

    정수삼각형 해결
    정수삼각형 해결

--- 6주차/정수_삼각형.cpp
+++ 6주차/정수_삼각형.cpp
@@ -1,13 +1,37 @@
 // 백준 1932번 문제
 
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
+int arr[502][502];
+
 int main()
 {
-	int n;
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, nb;
 	cin >> n;
+
+	for(int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n - (n - i); j++)
+		{
+			cin >> nb;
+			arr[i][j] = max(arr[i - 1][j - 1], arr[i - 1][j]) + nb;
+		}
+	}
+
+	// for(int i = 1; i <= n; i++)
+	// {
+	// 	for (int j = 1; j <= n - (n - i); j++)
+	// 		cout << arr[i][j] << ' ';
+	// 	cout << '\n';
+	// }
+
+	cout << *max_element(arr[n] + 1, arr[n] + n + 1); // 1인덱스로 세기때문에 +1을 해줘야함
 }
 /*
 		7
@@ -30,4 +54,10 @@ int main()
 
 매 요소마다 최소값을 고르는게 아니라 결과적으로 최소값이 되야함
 우선 왼쪽아래는 인덱스 - 1 오른쪽아래는 그대로
+
+그냥 1 인덱스로 두면 -1 했을때 0이되는 경우의
+예외처리를 안해줘도 됨
+어차피 0이랑 더한 값에서 max를 하면 값이 들어갈일이 없기때문
+
+min, max_element <- 주소값을 반환해주니 앞에 *를 붙여야함
 */
